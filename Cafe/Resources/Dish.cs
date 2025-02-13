2f5f78f062ce0c10699b4d66f2ead2bb44799197
Leonid Caminschi <leonid.caminschi@gmail.com>
2025-02-13 13:35:02 +0200

    Add project files.

---
+++ Cafe/Resources/Dish.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Resources
+{
+    internal class Dish
+    {
+        private
+        String name;
+        String description;
+        float price;
+        TimeSpan cookingTime;
+        List<Ingredient> ingredients;
+
+        public Dish(String name, String description, TimeSpan cookingTime, List<Ingredient> ingredients)
+        {
+            this.name = name;
+            this.description = description;
+            this.cookingTime = cookingTime;
+            this.ingredients = ingredients;
+        }
+
+        private float calcPrice()
+        {
+            if (this.price != 0)
+            {
+                return this.price;
+            }
+
+            float intermidiaryPrice = 0;
+            for (int i = 0; i < this.ingredients.Count; i++)
+            {
+                intermidiaryPrice += ingredients[i].getPrice();
+            }
+            this.price = intermidiaryPrice * 1.2f;
+            return this.price;
+
+        }
+    }
+}
---
+++ Cafe/Resources/Ingredient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Resources
+{
+    internal class Ingredient
+    {
+        private
+        String name;
+        float price;
+
+        public Ingredient(String name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public float getPrice()
+        {
+            return price;
+        }
+    }
+}
